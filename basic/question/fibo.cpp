553c51888845a0eb7f8d2d27a37ac9121ecd64b3
Nillotpal Mishra <nillmishra09@gmail.com>
2024-07-05 21:12:39 +0530

    basic changes

--- basic/question/fibo.cpp
+++ basic/question/fibo.cpp
@@ -4,14 +4,15 @@ int fibo(int n){
     if(n<=1){
         return n;
     }
-        int last = fibo(n-1);
-        int first = fibo(n-2);
+        int first = fibo(n-1);
+        int last = fibo(n-2);
     return last + first;  
 }
 
 int main(int argc, char const *argv[])
 {
-    int n = 4;
+    int n;
+    cin >> n;
     cout << fibo(n);
     return 0;
 }
